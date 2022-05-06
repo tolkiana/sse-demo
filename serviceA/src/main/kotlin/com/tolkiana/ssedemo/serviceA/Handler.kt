ab5180a23b10329165bf9e0cac730a3ccc1f14db
Nelida Velazquez <tolkiana@gmail.com>
2022-05-06 11:29:40 +0200

    Initial commit

---
+++ serviceA/src/main/kotlin/com/tolkiana/ssedemo/serviceA/Handler.kt
@@ -0,0 +1,4 @@
+package com.tolkiana.ssedemo.serviceA
+
+class Handler {
+}
---
+++ serviceA/src/main/kotlin/com/tolkiana/ssedemo/serviceA/ServiceAApplication.kt
@@ -0,0 +1,11 @@
+package com.tolkiana.ssedemo.serviceA
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceAApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceAApplication>(*args)
+}
